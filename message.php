4f8c0d313f65b4876096e6755c7bceae5839906c
jakirhossen1 <jakirhossen.nub@gmail.com>
2021-11-21 00:28:53 +0600

    Update message.php

--- message.php
+++ message.php
@@ -70,10 +70,32 @@
            </div>
        </div>
        <hr>
-       
-       <p>Uder Develop</p>
-     
-       
+       <div class="row">
+           <div class="col-md-3"></div>
+           <div class="col-md-6">
+               <div class="row">
+                   <div class="col-md-12">
+                       <input type="text" name="" class="form-control mt-3 mb-2" placeholder="Please enter your name" >
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="col-md-12">
+                       <input type="email" name="" class="form-control mt-3 mb-2" placeholder="Please enter your name" >
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="col-md-12">
+                       <textarea name="" class="form-control mt-3 mb-2 " placeholder="Please enter your messaage" rows="3"></textarea>
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="col-md-12">
+                       <input type="submit" name="" value="Send" class="btn btn-primary bx-pull-right">
+                   </div>
+               </div>
+           </div>
+           <div class="col-md-3"></div>
+       </div>
     </form>
     </div>
     </div>
