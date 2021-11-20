7924aecb3f01b88a693bfce5424505c6c4f47d30
jakirhossen1 <jakirhossen.nub@gmail.com>
2021-11-20 14:02:02 +0600

    Update attendanceSchedule.php

--- attendanceSchedule.php
+++ attendanceSchedule.php
@@ -97,7 +97,7 @@
                 </div>
                 <div class="row">
                     <div class="col-md-12">
-                        <input class="btn btn-primary mt-3 mb-3  " type="submit" name="" id="" value="Save">
+                        <input class="btn btn-primary mt-3 mb-3 bx-pull-right " type="submit" name="" id="" value="Save">
                     </div>
                 </div>
            </div>
