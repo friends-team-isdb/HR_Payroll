cb04c6a0e693dcb7ca5596d5e371dc69c79ea3b7
jakirhossen1 <jakirhossen.nub@gmail.com>
2021-11-20 13:12:43 +0600

    done

--- expenseList.php
+++ expenseList.php
@@ -70,27 +70,37 @@
            </div>
        </div>
        <hr>
+       <div class="row">
+           <div class="col-md-3">
+               
+           </div>
+           <div class="col-md-6">
+               <div class="row">
+                    <div class="col-md-12">
+                        Date:<input type="date" name="" id="" class="form-control mt-3 mb-3 ">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        Expense Description:<textarea  row="3"  name="" id="" class="form-control mt-3 mb-3 " placeholder="Plese write your expense description"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        Amount:<input type="number" name="" id="" class="form-control mt-3 mb-3 " placeholder="please enter your amount" >
+                    </div>
+                </div>
+                <div class="row">
+                   <div class="col-md-12">
+                       <input class="btn btn-primary mt-3 mb-3  " type="submit" name="" id="" value="Submit">
+                   </div>
+                </div>
+           </div>
+           <div class="col-md-3">
+               
+           </div>
+       </div>
        
-       <div class="row">
-           <div class="col-md-12">
-               Date:<input type="date" name="" id="" class="form-control mt-3 mb-3 ">
-           </div>
-       </div>
-       <div class="row">
-           <div class="col-md-12">
-               Expense Description:<textarea  row="3"  name="" id="" class="form-control mt-3 mb-3 " placeholder="Plese write your expense description"></textarea>
-           </div>
-       </div>
-       <div class="row">
-           <div class="col-md-12">
-               Amount:<input type="number" name="" id="" class="form-control mt-3 mb-3 " placeholder="please enter your amount" >
-           </div>
-       </div>
-        <div class="row">
-           <div class="col-md-12">
-               <input class="btn btn-primary mt-3 mb-3  " type="submit" name="" id="" value="Submit">
-           </div>
-       </div>
     </form>
     </div>
     </div>
