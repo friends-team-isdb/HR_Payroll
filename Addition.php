482ed16c9c0aa8954dfd357928d5ecf2e252cb57
SaidurSR <forumct24@gmail.com>
2021-11-22 16:48:24 +0600

    year problem

--- Addition.php
+++ Addition.php
@@ -1,3 +1,28 @@
+<?php 
+
+require "connect.php";
+if(isset($_POST['submit'])){
+    
+    $Additioncode=$_POST['Additioncode'];
+    $Description=$_POST['Description'];
+    $Amount=$_POST['Amount'];
+    $month=$_POST['month'];
+    $year=$_POST['year'];
+    $Status=$_POST['Status'];    
+
+    $s="SELECT addition_code FROM addition WHERE addition_code='$Additioncode'";
+    $qurey=mysqli_query($conn,$s);
+    $num=mysqli_num_rows($qurey);
+    if($num==1){
+        echo "Additon Already Exits";
+    }else{
+        $sql="INSERT INTO addition(addition_code,description,amount,month,addition_year,addition_status) Values('$Additioncode','$Description','$Amount','$month','$year','$Status')";
+        $qurey=mysqli_query($conn,$sql);
+        echo "Addition Completed!";
+    }   
+}
+?>
+
 <!doctype html>
 <html lang="en" class="light-theme">
 
@@ -74,28 +99,59 @@
        <div class="row">
         <div class="col-md-3"></div>
         <div class="col-md-6">
-          <input class="form-control mt-3" type="number" name="" id="" placeholder="Addition Code">
-          <textarea class="form-control mt-3" name=""placeholder="Description"></textarea>
-          <input class="form-control mt-3" type="number" name="" id="" placeholder="Amount">
+          <input class="form-control mt-3" type="number" name="Additioncode" id="" placeholder="Addition Code">
+          <textarea class="form-control mt-3" name="Description" placeholder="Description"></textarea>
+          <input class="form-control mt-3" type="number" name="Amount" id="" placeholder="Amount" required="required">
 
           <div class="row">
             <div class="col-md-12">
               <div class="row">
               <div class="col-md-6">
-                <input class="form-control mt-3" type="date" name="" id="">
+                <select class="form-control mt-3" name="month" id="">
+                  <option value="">Select Month</option>
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option value="June">June</option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="December">December</option>
+                </select>
               </div>
               <div class="col-md-6">
-                <input class="form-control mt-3" type="date" name="" id="">
+                <select class="form-control mt-3" name="year" id="">
+                  <option value="">Select Year</option>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                  <option value="2022">2022</option>
+                  <option value="2023">2023</option>
+                  <option value="2024">2024</option>
+                  <option value="2025">2025</option>
+                  <option value="2026">2026</option>
+                  <option value="2027">2027</option>
+                  <option value="2028">2028</option>
+                  <option value="2029">2029</option>
+                  <option value="2030">2030</option>
+                  
+                </select>  
               </div>             
             </div>
             </div>
             
           </div>
-          <select class="form-control mt-3" name="" id="">
+          <select class="form-control mt-3" name="Status" id="">
             <option value="">Status</option>
+            <option value="Aproved">Aproved</option>
+            <option value="Pending">Pending</option>
+
           </select>
           
-          <input type="submit" class="btn btn-primary mt-3 bx-pull-right" name="" id="" value="Save">
+          <input type="submit" class="btn btn-primary mt-3 bx-pull-right" name="submit" id="" value="Save">
 
         </div>
         <div class="col-md-3"></div>
