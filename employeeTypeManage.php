c0cd33e1159c634c1a4835e00d6b0d235fb635a7
SaidurSR <forumct24@gmail.com>
2021-11-22 19:29:33 +0600

    add btn done

--- employeeTypeManage.php
+++ employeeTypeManage.php
@@ -65,7 +65,15 @@
         
         <div class="row">
            <div class="col-md-12">
-               <h3 style="margin:10px;">Manage Employee Type</h3>
+             <div class="row">
+               <div class="col-md-6">
+                <h3 style="margin:10px;">Manage Employee Type</h3>
+               </div>
+               <div class="col-md-3"></div>
+               <div class="col-md-3">
+                 <a href="AddEmployeeType.php" class="btn btn-primary mt-3">Add Employee Type</a>
+               </div>
+              </div> 
            </div>
        </div>
        <hr>
