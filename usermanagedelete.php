37a97ee2b1777c92207d1bedab780bbbd9d75929
jakirhossen1 <jakirhossen.nub@gmail.com>
2021-11-23 01:41:07 +0600

    user delete

--- usermanagedelete.php
+++ usermanagedelete.php
@@ -2,7 +2,7 @@
     require "connect.php";
     
     $Id=$_GET['aid'];
-    $sql="delete from  user_table where user_id='$Id'";
+    $sql="delete from  user_table where full_name='$Id'";
     $query= mysqli_query($conn, $sql);
     header("location:userManage.php");
 ?>
