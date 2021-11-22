69f71e601dab0775e38b0bbbb45c47bda46d1ec8
Abdus Samad <63367158+abdussamad018@users.noreply.github.com>
2021-11-22 17:15:40 +0600

    some edit

--- addUser.php
+++ addUser.php
@@ -7,13 +7,14 @@ if(isset($_POST['submit'])){
     $email=$_POST['email'];
     $password=$_POST['password'];
     $status=$_POST['status'];
+    $accountCreation=date("Y-m-d");
     $s="SELECT * FROM user_table WHERE email='$email'";
     $result=mysqli_query($conn,$s);
     $num=mysqli_num_rows($result);
     if($num==1){
         echo "Email  Already Exits";
     }else{
-       $sqls="INSERT INTO `user_table`( `user_name`, `full_name`, `email`, `phone`, `passwords`,`user_status`)Values('$username','$fullname','$email','$phone','$password','$status')";
+       $sqls="INSERT INTO `user_table`( `user_name`, `full_name`, `email`, `phone`, `passwords`,`account_creation_date``user_status`)Values('$username','$fullname','$email','$phone','$password','$accountCreation',$status')";
     $query=mysqli_query($conn,$sqls);
         header("location:Desboard.php");
     }
