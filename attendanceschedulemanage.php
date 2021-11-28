80e63d01e169abe71a47318b7054f95b6114f6e8
jakirhossen1 <jakirhossen.nub@gmail.com>
2021-11-28 23:55:40 +0600

    done

--- attendanceschedulemanage.php
+++ attendanceschedulemanage.php
@@ -1,17 +1,6 @@
 <?php 
 require "connect.php";
-if(isset($_POST['submit'])){
-    
-    $signin=$_POST['signin'];
-    $signout=$_POST['signout'];
-    $latecount=$_POST['latecount'];
-    $absent=$_POST['absent'];
-    $sql="INSERT INTO attendance_schedule(signin_in,sign_out,late_count_time,absent_time) Values('$signin','$signout','$latecount','$absent')";
-    $qureyss=mysqli_query($conn,$sql);
-    header("location:attendanceschedulemanage.php");
-                     
-       
-}
+
 ?>
 <!doctype html>
 <html lang="en" class="light-theme">
@@ -76,54 +65,58 @@ if(isset($_POST['submit'])){
               
 <!--           Enter Your Code here-->
     <div class="modal-content">
-<div class="forms-body">
-    <form action="" method="post" enctype="multipart/form-data">
-       
-       <div class="row">
-           <div class="col-md-12">
-               <h3 style="margin:10px;">Attendance Schedule</h3>
+        <div class="forms-body">
+           <div class="row">
+               <div class="col-md-12">
+                   <h3 style="margin:10px;">Attendance Schedule Manage</h3>
+               </div>
            </div>
-       </div>
-       <hr>
-       <div class="row">
-           <div class="col-md-3">
-               
-           </div>
-           <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-12">
-                        Signin Time Setup:<input type="time" name="signin" class="form-control mt-3 mb-3 " >
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        Signout Time Setup:<input type="time" name="signout" class="form-control mt-3 mb-3 " >
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        Late Count Time:<input type="time" name="latecount" class="form-control mt-3 mb-3 "  >
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                       Absent Time: <input type="time" name="absent" class="form-control mt-3 mb-3 ">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <input class="btn btn-primary mt-3 mb-3 bx-pull-right " type="submit" name="submit" id="" value="Save">
-                    </div>
-                </div>
-           </div>
-           <div class="col-md-3">
-               
-           </div>
-       </div>
-    </form>
-    </div>
-    </div>
+           <hr>
+           <div class="row">
+                <div class="col-md-12 col-sm-12">
+                <a href="attendanceSchedule.php" class="btn btn-primary bx-pull-right mb-3">Add Attendance Schedule</a>
+                    <table class="table table-striped table-bordered mydataTable" >
+                        <tr>
+                            <th>SL</th>
+                            <th>Sign_In_Time</th>
+                            <th>Sign_Out_Time</th>
+                            <th>Late_Count_Time</th>
+                            <th>Absent_Time</th>
+                            <th>Action</th>
+                        </tr>
 
+                        <?php 
+                            $sl=1;
+                            $sql="SELECT * From attendance_schedule";
+
+                            $query=mysqli_query($conn,$sql);
+
+
+                            while($row=mysqli_fetch_array($query)){
+
+
+
+                        ?>
+                        <tr>
+                            <td><?php echo $sl++;?></td>
+                            <td><?php echo $row['Signin_in_time_setup'];?></td>
+                            <td><?php echo $row['Sign_out_time_setup'];?></td> 
+                            <td><?php echo $row['Late_Count_time'];?></td> 
+                            <td><?php echo $row['Absent_time'];?></td>
+                            <td> 
+                                <a class="btn btn-danger" href="?aid=<?php echo $row['Schedule_id']; ?>">Delete</a>
+                                <a class="btn btn-success" href="?aid=<?php echo $row['Schedule_id']; ?>">Update</a>
+                             </td>
+
+                        </tr>
+
+                        <?php }?>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
            
            
@@ -174,3 +167,4 @@ if(isset($_POST['submit'])){
 </body>
 
 </html>
+
