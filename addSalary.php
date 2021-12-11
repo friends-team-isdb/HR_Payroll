b257529b4e6ef660989dd5f49094bbbe3787cf61
jakirhossen1 <jakirhossen.nub@gmail.com>
2021-12-11 19:31:39 +0600

    add salary empty field check done

--- addSalary.php
+++ addSalary.php
@@ -70,7 +70,7 @@ if(!isset($_SESSION['userName'])){
             <!--           Enter Your Code here-->
             <div class="modal-content">
                 <div class="forms-body">
-                    <form action="" method="post" enctype="multipart/form-data">
+                    <form action="" method="post" enctype="multipart/form-data" id="myform">
                         <div class="row">
                             <div class="col-md-12">
                                 <h3 style="margin:10px;">Salary Setup</h3>
@@ -90,7 +90,8 @@ if(!isset($_SESSION['userName'])){
                                                 $query=mysqli_query($conn,$sql);
             $rowcount=mysqli_num_rows($query);
             ?>
-                                <select class="form-select" name="empNam" id="">
+                                <select class="form-select" name="empNam" id="empNam" onkeyup="change(this.id,'errempNam')" onblur="change(this.id,'errempNam')" >
+                                    <span id="errempNam"></span>
 
                                     <option value="">Select Employee</option>
 
@@ -112,7 +113,8 @@ if(!isset($_SESSION['userName'])){
                                                 $query=mysqli_query($conn,$sql);
             $rowcount=mysqli_num_rows($query);
             ?>
-                                <select class="form-select" name="salaryTyp" id="">
+                                <select class="form-select" name="salaryTyp" id="salaryTyp" onkeyup="change(this.id,'errsalaryTyp')" onblur="change(this.id,'errsalaryTyp')" >
+                                    <span id="errsalaryTyp"></span> 
 
                                     <option value="">Select Selary Type</option>
 
@@ -127,18 +129,17 @@ if(!isset($_SESSION['userName'])){
                 ?>
 
                                 </select>
-                                <input type="number" name="basicSalary" id="" class="form-control mt-3" placeholder="Basic Salary">
-
-                                <input type="number" name="medical" id="" class="form-control mt-3" placeholder="Medical">
-
-                                <input type="number" name="house" id="" class="form-control mt-3" placeholder="House Rent">
-
-                                <input type="number" name="food" id="" class="form-control mt-3" placeholder="Food">
-
-
-                                <input type="number" name="provi" id="" class="form-control mt-3" placeholder="Provident Fund">
-
-                                <input type="submit" name="submit" id="" class="btn btn-primary pull-right mt-3" value="Save">
+                                <input type="number" name="basicSalary" id="basicSalary" class="form-control mt-3" placeholder="Basic Salary" onkeyup="change(this.id,'errbasicSalary')" onblur="change(this.id,'errbasicSalary')" >
+                                <span id="errbasicSalary"></span>
+                                <input type="number" name="medical" id="medical" class="form-control mt-3" placeholder="Medical" onkeyup="change(this.id,'errmedical')" onblur="change(this.id,'errmedical')" >
+                                <span id="errmedical"></span>
+                                <input type="number" name="house" id="house" class="form-control mt-3" placeholder="House Rent" onkeyup="change(this.id,'errhouse')" onblur="change(this.id,'errhouse')" >
+                                <span id="errhouse"></span>
+                                <input type="number" name="food" id="food" class="form-control mt-3" placeholder="Food" onkeyup="change(this.id,'errfood')" onblur="change(this.id,'errfood')" >
+                                <span id="errfood"></span>
+                                <input type="number" name="provi" id="provi" class="form-control mt-3" placeholder="Provident Fund" onkeyup="change(this.id,'errprovi')" onblur="change(this.id,'errprovi')" >
+                                <span id="errprovi"></span>
+                                <input type="submit" name="submit" id="" class="btn btn-primary bx-pull-right mt-3" value="Save"  >
 
 
                             </div>
@@ -148,6 +149,103 @@ if(!isset($_SESSION['userName'])){
                     </form>
                 </div>
             </div>
+            
+<script type="text/JavaScript">
+   $("#myform").submit(function(){
+        var EmpNam= $("#empNam").val();
+        var SalaryTyp= $("#salaryTyp").val();
+        var BasicSalary= $("#basicSalary").val();
+        var Medical= $("#medical").val();
+        var House= $("#house").val();
+        var Food= $("#food").val();
+        var Provi= $("#provi").val();
+        
+      if(EmpNam==""){
+            $("#empNam").attr("style","border: 3px solid red");
+            $("#errempNam").html("This field must not be empty!");
+            return false;
+        }else{
+            $("#empNam").attr("style","border:");
+            $("#errempNam").html("");
+        }
+      if(SalaryTyp==""){
+            $("#salaryTyp").attr("style","border: 3px solid red");
+            $("#errsalaryTyp").html("This field must not be empty!");
+            return false;
+        }else{
+            $("#salaryTyp").attr("style","border:");
+            $("#errsalaryTyp").html("");
+        }
+      if(BasicSalary==""){
+            $("#basicSalary").attr("style","border: 3px solid red");
+            $("#errbasicSalary").html("This field must not be empty!");
+            return false;
+        }
+       else{
+           $("#basicSalary").attr("style","border:");
+           $("#errbasicSalary").html("");
+       }
+      if(Medical==""){
+            $("#medical").attr("style","border: 3px solid red");
+            $("#errmedical").html("This field must not be empty!");
+            return false;
+        }
+        else{
+            $("#medical").attr("style","border:");
+            $("#errmedical").html("");
+        }
+     if(House==""){
+            $("#house").attr("style","border: 3px solid red");
+            $("#errhouse").html("This field must not be empty!");
+            return false;
+        }else{
+            $("#house").attr("style","border:");
+            $("#errhouse").html("");
+        }
+      if(Food==""){
+            $("#food").attr("style","border: 3px solid red");
+            $("#errfood").html("This field must not be empty!");
+            return false;
+        }else{
+            $("#food").attr("style","border:");
+            $("#errfood").html("");
+        }
+      if(Provi==""){
+            $("#provi").attr("style","border: 3px solid red");
+            $("#errprovi").html("This field must not be empty!");
+            return false;
+        }
+       else{
+           $("#provi").attr("style","border:");
+           $("#errprovi").html("");
+       }
+     
+       
+    });
+    
+    function change(id,msg,type=null){
+        var get=$("#"+id).val();
+        
+        if(type==null){
+            if(get==""){
+            $("#"+id).attr("style","border: 3px solid red");
+            $("#"+msg).html("This field must not be empty!");
+            
+        }else{
+            $("#"+id).attr("style","border:");
+            $("#"+msg).html("");
+        }
+        
+        }
+        
+       
+     
+    }
+    
+</script>
+            
+            
+            
 
             <?php
             if(isset($_POST['submit'])){
